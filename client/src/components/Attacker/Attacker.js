854280c26ca750405d771bd9bd4be0a25beb90b9
ruprabh <ruprabh@gmail.com>
2024-03-13 14:05:31 +0530

    commit

--- client/src/components/Attacker/Attacker.js
+++ client/src/components/Attacker/Attacker.js
@@ -1,21 +1,35 @@
 import React from 'react'
-import AttackList from '../Data/AttackList.json'
+import AttackList from '../../Data/AttackList.json'
 import "./Attacker.css"
-
+// id , name , description , cost
+function AttackItem({item}){
+    return (
+        <div className="attack-card">
+            <div className='name'><h3>Name : {item.name}</h3></div>
+            <div className='description'>description: <p>
+                {item.description}
+                </p></div>
+            <div className='cost'>Cost : {item.cost}</div>
+        </div>
+    )
+}
 export default function Attacker() {
   return (
     <div className='attacker'>
-      <h1>Attacker Intro: </h1>
-      <p>Choose your attacker type</p>
-      
-      {
-        AttackList.map((item)=>{
-            return(
-            <p className = "atk-item" key = {item.id}>{item.name}</p>
-                    // <AttackItem/>
-            )
-        })
-      }
+        <h1>Attacker Intro: </h1>
+        <p>Choose your attacker type</p>
+        <div className='attacks'>
+            {
+            AttackList.map((item)=>{
+                return(
+                    <AttackItem 
+                    key={item.id} 
+                    item = {item}    
+                    />
+                    )
+                    })
+            }
+        </div>
     </div>
   )
 }
--- client/src/components/Defender/Defender.js
+++ client/src/components/Defender/Defender.js
@@ -1,12 +1,37 @@
 import React from 'react'
-
+import "./Defender.css"
+import DefenseList from "../../Data/DefendList.json"
 //here I have to add options for Defender
+function DefenseItem({item}){
+    return (
+        <div className="defense-card">
+            <div className='name'><h3>Name : {item.name}</h3></div>
+            <div className='description'>description: <p>
+                {item.description}
+                </p></div>
+            <div className='cost'>Cost : {item.cost}</div>
+        </div>
+    )
+}
 function Defender() {
-  return (
-    <div>
-      Hello from Defender
-    </div>
-  )
+    return (
+        <div className='defender'>
+            <h1>Defender Intro: </h1>
+            <p>Choose your Defense type</p>
+            <div className='defense'>
+                {
+                DefenseList.map((item)=>{
+                    return(
+                        <DefenseItem 
+                        key={item.id} 
+                        item = {item}    
+                        />
+                        )
+                        })
+                }
+            </div>
+        </div>
+      )
 }
 
 export default Defender
